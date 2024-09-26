0b30dc82d2cdf656d902c497277f9b17431151f1
wremert <christian@wremert.com>
2024-09-26 20:20:33 +0200

    Tested a page endpoint

--- backend/main.go
+++ backend/main.go
@@ -6,6 +6,12 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type dataChunk struct {
+	DATA     []joke       `json:"data"`
+	TYPE     string       `json:"type"`
+	METADATA pageMetaData `json:"pagemetadata"`
+}
+
 type joke struct {
 	ID        string `json:"id"`
 	TELLING   string `json:"telling"`
@@ -13,18 +19,43 @@ type joke struct {
 	CATEGORY  string `json:"category"`
 }
 
+type pageMetaData struct {
+	ID          string `json:"id"`
+	PAGETITLE   string `json:"pagetitle"`
+	DESCRIPTION string `json:"description"`
+	LOGO        string `json:"logo"`
+}
+
 var jokes = []joke{
 	{ID: "1", TELLING: "Jag har börjat säga moucho till mina spanska vänner...", PUNCHLINE: "...Det betyder mycket för dom.", CATEGORY: "HRAPPROVED"},
 	{ID: "2", TELLING: "Vad är det för likhet mellan en nyförlöst mamma och någon som kommer i tid till bussen?", PUNCHLINE: "Slapp springa", CATEGORY: "18+"},
+}
+
+var pageMetaDataObject = pageMetaData{
+	ID:          "P1",
+	PAGETITLE:   "Världens bästa skämt",
+	DESCRIPTION: "Pagetitlen beskriver bra",
+	LOGO:        "https://logoipsum.com/artwork/327",
+}
+
+var dataChunkObject = dataChunk{
+	DATA:     jokes,
+	TYPE:     "PAGE",
+	METADATA: pageMetaDataObject,
 }
 
 func getJokes(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, jokes)
 }
 
+func getPage(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, dataChunkObject)
+}
+
 func main() {
 	router := gin.Default()
 	router.GET("/jokes", getJokes)
+	router.GET("/page", getPage)
 
 	router.Run("localhost:8000")
 }
