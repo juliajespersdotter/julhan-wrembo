c80cf5b5984d95b77397f4512baa8a577125d7af
Julia <julia.jespersdotter@gmail.com>
2024-10-30 19:04:12 +0100

    mongo db connection

--- backend/main.go
+++ backend/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type dataChunk struct {
@@ -123,6 +128,24 @@ func createCategory(c *gin.Context) {
 }
 
 func main() {
+	// Use the SetServerAPIOptions() method to set the version of the Stable API on the client
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().ApplyURI("mongodb+srv://adminhogman:123@cluster0.4d6yn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").SetServerAPIOptions(serverAPI)
+	// Create a new client and connect to the server
+	client, err := mongo.Connect(context.TODO(), opts)
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+		panic(err)
+		}
+	}()
+  // Send a ping to confirm a successful connection
+	if err := client.Database("admin").RunCommand(context.TODO(), bson.D{{"ping", 1}}).Err(); err != nil {
+		panic(err)
+	}
+  	fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
     router := gin.Default()
 	// GET general
     router.GET("/jokes", getJokes)
